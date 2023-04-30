b5a6444a7f41a704e30cdfb9b1626f09f378dbfe
Daniel Pires <dmateusp@gmail.com>
2023-04-30 09:49:08 +0100

    Add example that reproduces proto issue

---
+++ example/schema/user.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+	"github.com/cloneable/rules_ent/example/protos"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ent.Schema
+}
+
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("email").Unique(),
+		field.Bytes("preferences").GoType(&protos.UserSettings{}),
+	}
+}
+
+func (User) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.Time{},
+	}
+}
+
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{}
+}
